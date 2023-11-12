c60816c2b79b9cd302bcfa0dca5a43835dd78aa4
Gytis Ptašinskas <51530980+gytisptasinskas@users.noreply.github.com>
2023-11-12 15:36:53 +0200

    Initial Commit

---
+++ Assistant/AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantApp.swift
+//  Assistant
+//
+//  Created by Gytis Ptašinskas on 12/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assistant
+//
+//  Created by Gytis Ptašinskas on 12/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
